14af64134c75d126ebac406a3c73ba409579ca15
barsn136 <barsn136@gmail.com>
2023-01-08 14:24:53 +0200

    create default rds

--- rds.tf
+++ rds.tf
@@ -7,33 +7,18 @@ resource "aws_db_parameter_group" "blue_green_parameter_group" {
     }
 }
 
-# resource "aws_db_instance" "rds_Instance" {
-#   db_name                = "test_rds_db"
-#   allocated_storage      = 10
-#   max_allocated_storage  = 20
-#   engine                 = "mysql"
-#   engine_version         = "8.0"
-#   instance_class         = "db.t3.micro"
-#   username               = "admin"
-#   password               = "!Andrey1989"
-#   parameter_group_name   = "default.mysql8.0"
-#   db_subnet_group_name   = aws_db_subnet_group.db_subnet_gp_name.name
-#   skip_final_snapshot    = true
-#   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-# }
-
-
-
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
-  identifier = "testdbbluegreen"
-  engine = "mysql"
-  engine_version = "8.0"
-  instance_class         = "db.t3.micro"
+resource "aws_db_instance" "rds_Instance" {
+  db_name                = "test_rds_db"
   allocated_storage      = 10
   max_allocated_storage  = 20
-  db_name = "testdbbluegreen"
-  username = "admin"
-  password = "!Andrey1989"
-  parameter_group_name = aws_db_parameter_group.blue_green_parameter_group.id
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = "!Andrey1989"
+  parameter_group_name   = "default.mysql8.0"
+  skip_final_snapshot    = true
 }
+
+
+
