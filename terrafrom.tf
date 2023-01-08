5b064d40dca990648fdfda4833c90207b707e14e
barsn136 <barsn136@gmail.com>
2023-01-08 13:38:08 +0200

    added rds f

---
+++ terrafrom.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  cloud {
+    organization = "TeraSky"
+
+    workspaces {
+      name = "rds-blue-green-deployment"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.48.0"
+    }
+
+  }
+
+  required_version = ">= 1.1.0"
+}
+provider "aws" {
+    region = "eu-west-2"
+  
+}
