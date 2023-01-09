9ece6580b5a410f99c40fa05b30d95e4f901be6e
barsn136 <barsn136@gmail.com>
2023-01-09 14:31:58 +0200

    fgh

--- rds.tf
+++ rds.tf
@@ -29,7 +29,7 @@ output "name" {
 
 
 resource "null_resource" "blue_green_update" {
-  provisioner "remote-exec" {
+  provisioner "script" {
     command = "aws rds create-blue-green-deployment --blue-green-deployment-name my-blue-green-deployment --source arn:aws:rds:eu-west-2:113265635309:db:terraform-20230109122215719500000001 --target-engine-version 8.0.28 --target-db-parameter-group-name default.mysql8.0"
   }
 }
