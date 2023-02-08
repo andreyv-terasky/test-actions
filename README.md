742b1fd4c78ebab8dee054fa92d27a61227e3687
barsn136 <barsn136@gmail.com>
2023-02-08 11:38:25 +0200

    create rds + bgd

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# rds-blue-green-deployment
+# ################################################################################
+# Create Blue Green Deployment for RDS
+# ################################################################################
+
+
+1. To create blue/green environmnet uncomment "Create - Blue Green Environment" and run terraform.
+
+2. To perform switch over deployment you need:
+    1. Comment create block "Create - Blue Green Environment".
+    2. Uncomment describe block "Describe - get bgd-id".
+    3. Uncomment switch block "Switch Over - Switch green(stage) with blue(production".
+    4. Run terraform.
+
+3. To Delete blue/green environmnet you need:
+    1. Comment describe block "Describe - get bgd-id".
+    2. Comment switch block "Switch Over - Switch green(stage) with blue(production".
+    3. Run terraform.
+    4. Uncomment describe block "Describe - get bgd-id".
+    5. Uncomment delete block "Delete Deployment":
+        1. If you did a switch-over you need run with --no-delete-target parameter (Not deleting green)
+        2. If you didn't a switch-over you need run with --delete-target parameter (Deleting green)
--- blue-green_v2.tf
+++ blue-green_v2.tf
@@ -1,6 +1,6 @@
-# ################################################################################
-# # Create Blue Green Deployment for RDS
-# ################################################################################ 
+################################################################################
+# Create Blue Green Deployment for RDS
+################################################################################ 
 
 locals {
   region                     = "eu-west-1"
@@ -16,76 +16,76 @@ locals {
 # # Create RDS
 # ################################################################################ 
 
-# resource "aws_db_instance" "default" {
-#   allocated_storage       = 10
-#   db_name                 = "mydb"
-#   engine                  = "mysql"
-#   engine_version          = "5.7"
-#   instance_class          = "db.t3.micro"
-#   username                = "admin"
-#   password                = "Andrey90!"
-#   parameter_group_name    = "default.mysql5.7"
-#   skip_final_snapshot     = true
-#   backup_retention_period = 1
-#   backup_window           = "09:00-10:00"
-#   blue_green_update {
-#     enabled = true
-#   }
-# }
+resource "aws_db_instance" "default" {
+  allocated_storage       = 10
+  db_name                 = "mydb"
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t3.micro"
+  username                = "admin"
+  password                = "Andrey90!"
+  parameter_group_name    = "default.mysql5.7"
+  skip_final_snapshot     = true
+  backup_retention_period = 1
+  backup_window           = "09:00-10:00"
+  blue_green_update {
+    enabled = true
+  }
+}
 
 # ################################################################################
 # # Create - Blue Green Environment
 # ################################################################################ 
 
-# resource "null_resource" "create_bgd" {
-#   provisioner "local-exec" {
-#     command = "aws rds create-blue-green-deployment --blue-green-deployment-name $f_blue_green_name --source $f_source_db --target-db-parameter-group-name $f_target_db_parameter_group --output $f_output --region $f_region"
-#     environment = {
-#       f_blue_green_name           = local.blue-green-deployment-name
-#       f_source_db                 = aws_db_instance.default.arn
-#       f_target_db_parameter_group = aws_db_instance.default.parameter_group_name
-#       f_region                    = local.region
-#       f_output                    = local.output
-#     }
-#   }
-# }
+resource "null_resource" "create_bgd" {
+  provisioner "local-exec" {
+    command = "aws rds create-blue-green-deployment --blue-green-deployment-name $f_blue_green_name --source $f_source_db --target-db-parameter-group-name $f_target_db_parameter_group --output $f_output --region $f_region"
+    environment = {
+      f_blue_green_name           = local.blue-green-deployment-name
+      f_source_db                 = aws_db_instance.default.arn
+      f_target_db_parameter_group = aws_db_instance.default.parameter_group_name
+      f_region                    = local.region
+      f_output                    = local.output
+    }
+  }
+}
 
 ################################################################################
 # Describe - get bgd-id 
 ################################################################################
 
-resource "null_resource" "desccribe" {
-  provisioner "local-exec" {
-    command = "aws rds describe-blue-green-deployments --filters Name=blue-green-deployment-name,Values=$f_blue_green_name --region $f_region --output $f_output > example.json"
-    //command = "aws rds describe-blue-green-deployments --filters Name=blue-green-deployment-name,Values=$f_blue_green_name --region $f_region > output.json"
-    environment = {
-      f_blue_green_name = local.blue-green-deployment-name
-      f_region          = local.region
-      f_output          = local.output
-    }
-  }
-}
+# resource "null_resource" "desccribe" {
+#   provisioner "local-exec" {
+#     command = "aws rds describe-blue-green-deployments --filters Name=blue-green-deployment-name,Values=$f_blue_green_name --region $f_region --output $f_output > example.json"
+#     //command = "aws rds describe-blue-green-deployments --filters Name=blue-green-deployment-name,Values=$f_blue_green_name --region $f_region > output.json"
+#     environment = {
+#       f_blue_green_name = local.blue-green-deployment-name
+#       f_region          = local.region
+#       f_output          = local.output
+#     }
+#   }
+# }
 
-data "local_file" "example" {
-  filename = "${path.module}/example.json"
-  depends_on = [
-    null_resource.desccribe
-  ]
-}
+# data "local_file" "example" {
+#   filename = "${path.module}/example.json"
+#   depends_on = [
+#     null_resource.desccribe
+#   ]
+# }
 
-locals {
-  bgd_id = {
-    id = jsondecode(data.local_file.example.content)["BlueGreenDeployments"][0]["BlueGreenDeploymentIdentifier"]
-  }
-}
+# locals {
+#   bgd_id = {
+#     id = jsondecode(data.local_file.example.content)["BlueGreenDeployments"][0]["BlueGreenDeploymentIdentifier"]
+#   }
+# }
 
-output "bgd_id" {
-  value = local.bgd_id.id
-}
+# output "bgd_id" {
+#   value = local.bgd_id.id
+# }
 
 
 ################################################################################
-# Switch Over - Switch green(stage)
+# Switch Over - Switch green(stage) with blue(production)
 ################################################################################
 
 # resource "null_resource" "switch" {
@@ -103,19 +103,19 @@ output "bgd_id" {
 # # Delete Deployment
 # ################################################################################
 
-resource "null_resource" "delete" {
-  provisioner "local-exec" {
-    # For Non completed switch over
-    command = "aws rds delete-blue-green-deployment --blue-green-deployment-identifier $f_BlueGreenDeploymentIdentifier --delete-target --region $f_region --output $f_output"
-    environment = {
-      f_BlueGreenDeploymentIdentifier = local.bgd_id.id
-      f_region                        = local.region
-      f_output                        = local.output
-    }
-    # Forcompleted switch over
-    //command = "aws rds delete-blue-green-deployment --blue-green-deployment-identifier $f_BlueGreenDeploymentIdentifier --no-delete-target --region eu-west-1 --output json"
-  }
-}
+# resource "null_resource" "delete" {
+#   provisioner "local-exec" {
+#     # For Non completed switch over
+#     command = "aws rds delete-blue-green-deployment --blue-green-deployment-identifier $f_BlueGreenDeploymentIdentifier --delete-target --region $f_region --output $f_output"
+#     environment = {
+#       f_BlueGreenDeploymentIdentifier = local.bgd_id.id
+#       f_region                        = local.region
+#       f_output                        = local.output
+#     }
+#     # Forcompleted switch over
+#     //command = "aws rds delete-blue-green-deployment --blue-green-deployment-identifier $f_BlueGreenDeploymentIdentifier --no-delete-target --region eu-west-1 --output json"
+#   }
+# }
 
 
 # # Import Green (Old DB instnce)
